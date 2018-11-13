5b6a03bcb37645aa4556d57dd6e1697ca1d584b5
bhumika shukla <bhumika.shukla@vendortopay.com>
2018-11-13 11:39:17 +0530

    Initial Commit
    Initial Commit

---
+++ V2P/App_Start/BundleConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+namespace V2P.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                          "~/js/bootstrap.js",
+                          "~/js/bootstrap.min.js",
+                          "~/js/npm.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/css/bootstrap.css",
+                      "~/Content/css/carousel.css",
+                      "~/Content/css/style.css"));
+        }
+    }
+}
